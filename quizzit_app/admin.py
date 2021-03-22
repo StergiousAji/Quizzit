ff95a026d2fe526c9e042a4bc7f4525b12d63def
Eric2767 <2465059c@student.gla.ac.uk>
2021-03-22 13:12:46 +0000

    Initial database

--- quizzit_app/admin.py
+++ quizzit_app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from quizzit_app.models import Register_User, Category, Quiz, Record
 
-# Register your models here.
+class RegisterUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'password', 'admin')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ('quizID', 'name', 'difficulty', 'questions', 'category')
+
+class RecordAdmin(admin.ModelAdmin):
+    list_display = ('username', 'quizID', 'score')
+
+admin.site.register(Register_User, RegisterUserAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Quiz, QuizAdmin)
+admin.site.register(Record, RecordAdmin)
--- quizzit_app/models.py
+++ quizzit_app/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
-# Create your models here.
+
+class Register_User(models.Model):
+    username = models.CharField(max_length=30, unique=True)
+    email = models.CharField(max_length=60)
+    password = models.CharField(max_length=35)
+    admin = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = 'Register Users'
+
+    def __str__(self):
+        return self.username
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+
+class Quiz(models.Model):
+    EASY = 'Easy'
+    MED = 'Medium'
+    DIFF = 'Difficult'
+    difficulty_choices = [(EASY, 'Easy'), (MED, 'Medium'), (DIFF, 'Difficult')]
+
+    quizID = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=30)
+    difficulty = models.CharField(max_length=15, choices=difficulty_choices)
+    questions = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+    class Meta:
+        verbose_name_plural = 'Quizzes'
+
+    def __str__(self):
+        return f'({self.quizID}, {self.name}, {self.category})'
+
+
+
+class Record(models.Model):
+    username = models.ForeignKey(Register_User, on_delete=models.CASCADE)
+    quizID = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    score = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name_plural = 'Records'
+
+    def __str__(self):
+        return f'{self.username}, {self.quizID}'  
