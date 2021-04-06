9ee65fec3c7bfd6a6cc9de29bb5a141282afaddd
Eric2767 <2465059c@student.gla.ac.uk>
2021-04-06 15:11:53 +0100

    updated settings

--- Quizzit/settings.py
+++ Quizzit/settings.py
@@ -23,7 +23,11 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bkmxx0i341+qy6a7q78=ki=gm*cepww7bihtwp!5rv^r$o2n%b'
+key = None
+with open('secret.key') as f:
+    key = f.read().strip()  
+SECRET_KEY = key
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
